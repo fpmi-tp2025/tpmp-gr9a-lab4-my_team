6dbc62822f7a46aff506c7aae59386a0ab3f67f8
ignat <lazovikignat@gmail.com>
2025-05-01 21:21:32 +0300

    Add User and UserRole classes

--- src/main/java/flight/Main.java
+++ src/main/java/flight/Main.java
@@ -1,10 +1,5 @@
 package flight;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/main/java/flight/User.java
@@ -0,0 +1,4 @@
+package flight;
+
+public record User(UserRole role, int pilotId, int helicopterId) {
+}
